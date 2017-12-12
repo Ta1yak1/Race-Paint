b78089b18fe8b14b6b50bff58376f758969ccd01
Jay Yang <hjyang1@uci.edu>
2017-12-12 10:29:18 -0800

    Created basic working server.js

--- server.js
+++ server.js
@@ -2,3 +2,26 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+app.use(express.static("app/public"));
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+// Starts the server to begin listening
+// =============================================================
+io.on('connection', function (client) {
+    client.on('event', function (data) { });
+    client.on('disconnect', function () { });
+});
+
+server.listen(PORT, function(){
+    console.log('Running on Port '+PORT+'...');
+});
+
