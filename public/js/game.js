6b528c7e5b3a1e1d4549582f2fcecd92c11c3094
Leo <zenlionel@gmail.com>
2017-12-13 21:34:33 -0800

    Game not tested yet, bmd not set
    bmd not set

--- public/js/game.js
+++ public/js/game.js
@@ -13,20 +13,33 @@ racePaint.Game.prototype = {
 
         this.player.scale.setTo(1.5);
 
-        
+
         //TODO set the animation for the car moving, I can't remember what it was.
-       //something along the lines of
-       //this.player.animations.add('drive',[1], 1, true);
-       //this.player.animations.play('drive');
-        
+        //something along the lines of
+        //this.player.animations.add('drive',[1], 1, true);
+        //this.player.animations.play('drive');
+
         //set collision with the world border, world might be larger than our tilemap currently
         this.game.physics.P2JS.enable(this.player);
+        
         this.playerSpeed = 50;
+
         this.player.body.collideWorldBounds = true;
+
+        game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
     },
     update: function () {
-        if(this.game.input.activePointer.justPressed()){
-            this.game.physics.P2JS
+        if (arrowKeys.left.isDown) {
+            this.player.body.rotateLeft(100);
+        } else if (arrowKeys.right.isDown) {
+            this.player.body.rotateRight(100);
+        } else {
+            this.player.body.setZeroRotation();
         }
-    },
+        if (arrowKeys.up.isDown) {
+            this.player.body.thrust(150);
+        } else if (arrowKeys.down.isDown) {
+            this.player.body.reverse(100);
+        }
+    }
 };
