63af7955b432cca442f814efcc9b01979d3629cf
Jay Yang <hjyang1@uci.edu>
2017-12-15 11:33:07 -0800

    Cleaned up Code and Comments to make code more readable

--- public/js/client.js
+++ public/js/client.js
@@ -1,77 +1,85 @@
-
 var Client = {};
 Client.socket = io.connect();
 
+//Client methods emit signals to server============================
+
 Client.askNewPlayer = function () {
     Client.socket.emit('newPlayerConnect');
-    console.log('askNewPlayer');
-}
+    console.log('New Player Connected');
+};
 
+Client.updateMe = function () {
+    Client.socket.emit('update_Me', Game.self.id, 
+        Game.self.sprite.x, Game.self.sprite.y)
+};
+
+//Player movement---------------------------------------------------
+Client.sendUp = function () {
+    Client.socket.emit('upKey');
+};
+
+Client.sendDown = function () {
+    Client.socket.emit('downKey');
+};
+
+Client.sendLeft = function () {
+    Client.socket.emit('leftKey');
+};
+
+Client.sendRight = function () {
+    Client.socket.emit('rightKey');
+};
+
+Client.sendStill = function () {
+    Client.socket.emit('noneClick');
+};
+
+//===================================================================
+
+
+//Listens for emits from server =====================================
 Client.socket.on('addSelf', function(id,x,y){
     Game.addSelf(id,x,y);
 });
 
-Client.socket.on('addOthers', function(data){
+Client.socket.on('otherPlayer', function (id,x,y) {
+    Game.addOtherPlayer(id,x,y);
+});
+
+Client.socket.on('updateMeToAll', function (id,x,y) {
+    Game.updateOthers(id,x,y);
+});
+
+Client.socket.on('addAllOthers', function(data){
     for (var i = 0; i < data.length; i++){
-        Game.addNewPlayer(data[i].id, data[i].x, data[i].y);
+        Game.addOtherPlayer(data[i].id, 
+            data[i].x, data[i].y);
     }
 });
 
-Client.sendTest = function () {
-    console.log('test sent from client.js');
-    Client.socket.emit('test');
-}
-//take data from server and create Client newPlayer
-Client.socket.on('newPlayer', function (id,x,y) {
-    Game.addNewPlayer(id,x,y);
-});
-
-//delete player based on id passed from server
 Client.socket.on('remove', function (id) {
     Game.removePlayer(id);
 });
 
-//sending button click of player
-// Client.sendButton = function (arrowKeys) {
-//     Client.socket.emit('button', arrowKeys);
-// };
-
+//Player movement---------------------------------------------------
 Client.socket.on('moveUp', function (data) {
     Game.pressUp(data.id);
 });
+
 Client.socket.on('moveDown', function (data) {
     Game.pressDown(data.id);
 });
+
 Client.socket.on('moveLeft', function (data) {
     Game.pressLeft(data.id);
 });
+
 Client.socket.on('moveRight', function (data) {
     Game.pressRight(data.id);
 });
+
 Client.socket.on('moveNone', function (data) {
     Game.pressNone(data.id);
 });
-Client.sendUp = function () {
-    Client.socket.emit('upKey');
-}
-Client.sendDown = function () {
-    Client.socket.emit('downKey');
-}
-Client.sendLeft = function () {
-    Client.socket.emit('leftKey');
-}
-Client.sendRight = function () {
-    Client.socket.emit('rightKey');
-}
-Client.sendStill = function () {
-    Client.socket.emit('noneClick');
-}
 
-
-Client.updateMe = function () {
-    Client.socket.emit('update_Me', Game.self.id, Game.self.sprite.x, Game.self.sprite.y)
-}
-
-Client.socket.on('updateMeToAll', function (id,x,y) {
-    Game.updateOthers(id,x,y);
-})
+//===================================================================
